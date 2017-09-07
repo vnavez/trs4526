549a5d0636f8071b0905e5c1b184904519193d76
victor <v.navez@gmail.com>
2017-09-07 19:16:01 +0200

    Fix transfer

--- src/FrontBundle/EventListener/TorrentSubscriber.php
+++ src/FrontBundle/EventListener/TorrentSubscriber.php
@@ -58,20 +58,19 @@ class TorrentSubscriber implements EventSubscriber {
             $entity = $entity->getTorrent();
         }
 
-        $user_id = $entity->getUser() ? $entity->getUser()->getId() : null;
-
         $response = array(
             'id' => $entity->getId(),
             'id_user' => $entity->getUser() ? $entity->getUser()->getId() : null,
-            'action' => $action,
-            'html' => $action != 'delete' ? $this->container->get('templating')->render('FrontBundle:torrent:torrent_line.html.twig', array('torrent' => $entity, 'user_id' => $user_id)) : ''
+            'action' => $action
         );
 
         $pusher = $this->container->get('gos_web_socket.wamp.pusher');
 
         if (isset($transfer) && $transfer->getUser()) {
+            $response['html'] = $action != 'delete' ? $this->container->get('templating')->render('FrontBundle:torrent:torrent_line.html.twig', array('torrent' => $entity, 'user_id' => $transfer->getUser()->getId())) : '';
             $pusher->push($response, 'torrent_update_client', array('user_id' => $transfer->getUser()->getId()));
         } else {
+            $response['html'] = $action != 'delete' ? $this->container->get('templating')->render('FrontBundle:torrent:torrent_line.html.twig', array('torrent' => $entity, 'user_id' => null)) : '';
             $pusher->push($response, 'torrent_update');
         }
 
